9590a5994e35611ccc7a9eab13b43aa23507de3e
jev-uv <jev@ulverstonvictoria.cumbria.sch.uk>
2016-02-29 22:04:01 +0000

    Changed levels and a few extra options

--- levels/practice.js
+++ levels/practice.js
@@ -1,20 +1,13 @@
 bt.addLevel({
-	controller:bt.CONTROLLER_MANUAL, 
-	tank: 
-		{
-			x:5, y:5, 
-			name:"You", color:"rgba(0, 255, 0,0.8)", 
-			code:"Ly8gZXhhbXBsZSBwcm9ncmFtIHRvIGFkZCB0d28gbnVtYmVycyB0b2dldGhlcg0KDQovLyBpbnN0cnVjdGlvbnM6DQoNCglJTlAgICAgICAgICAvLyBhc2sgZm9yIGEgbnVtYmVyDQoJU1RBIDggICAgICAgLy8gc3RvcmUgaXQgYXQgYWRkcmVzcyA4DQoJSU5QICAgICAgICAgLy8gYXNrIGZvciBhbm90aGVyIG51bWJlcg0KCVNUQSA5ICAgICAgIC8vIHN0b3JlIGl0IGF0IGFkZHJlc3MgOQ0KCUxEQSA4ICAgICAgIC8vIGxvYWQgbnVtYmVyIGZyb20gYWRkcmVzcyA4DQoJQUREIDkgICAgICAgLy8gYWRkIGl0IHRvIGFkZHJlc3MgOQ0KCU9VVCAgICAgICAgIC8vIGRpc3BsYXkgcmVzdWx0DQogICAgSExUICAgICAgICAgLy8gc3RvcCB0aGUgcHJvZ3JhbQ0KICAgIA0KLy8gZGF0YTogICANCg0KICAgIERBVCAwICAgICAgIC8vIHN0b3JlcyBmaXJzdCBudW1iZXINCiAgICBEQVQgMCAgICAgICAvLyBzdG9yZXMgc2Vjb25kIG51bWJlcg=="
-		}, 
-	title: "Practice level", 
-	description: "Use the manual control pad to move the tank to collect the checkpoints.",
-		
+	controller: bt.CONTROLLER_LITTLE_MAN_COMPUTER,
+	tank: {
+		x:2, y:4,
+		name: "You", color: "rgba(0, 255,0,0.8)",
+		code: "Ly8gQ2xpY2sgQXNzZW1ibGUgYW5kIFJ1bg0KLy8gdG8gc2VlIHRoZSBwcm9ncmFtIGV4ZWN1dGUNCg0KLy8gSW5zdHJ1Y3Rpb25zDQoJTERBIEZvcndhcmQgLy8gTG9hZCBGb3J3YXJkICgwKQ0KCU9VVCAxICAgICAgIC8vIE91dHB1dCBvbiBNb3ZlbWVudCBjaGFubmVsDQoJT1VUIDEgICAgICAgLy8gT3V0cHV0IG9uIE1vdmVtZW50IGNoYW5uZWwNCglPVVQgMSAgICAgICAvLyBPdXRwdXQgb24gTW92ZW1lbnQgY2hhbm5lbA0KCUhMVCAgICAgICAgIC8vIFN0b3AgdGhlIHByb2dyYW0NCg0KLy8gRGF0YSBsYWJlbHMNCi8vIENoYW5uZWwgMSAtIG1vdmVtZW50DQpGb3J3YXJkICAgICBEQVQgMA0KQmFja3dhcmQgICAgREFUIDENCg=="
+		},
+	title: "Practice",
+	description: "<p>This level lets you see how Battle Tanks works.</p><p>Click on the label above the tank to see the assembly code that has been written for you.</p><p>The aim of this first level is to move 3 squares to the north.</p>",
 	tasks:[
-		[5,1, bt.ITEM_TYPE_CHECKPOINT, bt.ACTION_COLLECT],
-		[1,1, bt.ITEM_TYPE_CHECKPOINT, bt.ACTION_COLLECT],
-		[1,2, bt.ITEM_TYPE_FUEL, bt.ACTION_COLLECT],
-		[1,5, bt.ITEM_TYPE_CHECKPOINT, bt.ACTION_COLLECT]
-		
-		]
-	});
-	
+		[2, 1, bt.ITEM_TYPE_CHECKPOINT, bt.ACTION_COLLECT]
+	]
+});
